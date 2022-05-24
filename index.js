218c8b40876afb714e0c3583a47a1bf781ca450f
chrstno040420 <104917222+chrstno040420@users.noreply.github.com>
2022-05-24 15:22:24 -0500

    commit tree c3efc070c946f2ab1eb20d09c496f3b0f6eefdec

---
+++ index.js
@@ -0,0 +1,26 @@
+function shout(string) {
+    return string.toUpperCase();
+}
+
+function whisper(string){
+    return string.toLowerCase();
+}
+
+function logShout(string){
+    console.log(string.toUpperCase());
+}
+
+function logWhisper(string){
+    console.log(string.toLowerCase());
+}
+
+function sayHiToHeadphonedRoommate(string){
+    if string.toLowerCase
+        console.log(" I can\'t hear you!");
+    if string.toUpperCase
+        console.log("YES INDEED!");
+
+    if sayHiToHeadphonedRoommate("Let\'s have dinner together!") 
+        console.log("I would love to!");
+}   
+
